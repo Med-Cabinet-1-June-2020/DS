f75d47b7713abcbc1313b143cf3a7df03fe68760
Kristine <ying.kristine.wang@gmail.com>
2020-06-23 11:57:49 -0400

    Added connection and cursor information

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,19 +1,46 @@
 from flask import Flask
-from web_app.routes.med_routes import med_routes
+# from web_app.routes.med_routes import med_routes
+import psycopg2
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-def create_app():
-    app = Flask(__name__)
+DB_NAME=os.getenv("DB_NAME")
+DB_USER=os.getenv("DB_USER")
+DB_PASSWORD=os.getenv("DB_PASSWORD")
+DB_HOST=os.getenv("DB_HOST")
 
-    # app.config["SECRET_KEY"] = SECRET_KEY
-    # app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
-    # # db.init_app(app)
-    # # migrate.init_app(app, db)
+print(DB_NAME,DB_USER, DB_PASSWORD, DB_HOST)
 
-    app.register_blueprint(med_routes)
+connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
+print(connection)
 
-    return app
+cursor = connection.cursor()
+print(cursor)
 
-if __name__ == "__main__":
-    my_app = create_app()
-    my_app.run(debug=True)
+cursor.execute("SELECT * FROM medcabinet1;")
+
+result = cursor.fetchall()
+print(result)
+
+# to save transactions
+connection.commit()
+cursor.close()
+connection.close()
+
+# def create_app():
+#     app = Flask(__name__)
+
+#     # app.config["SECRET_KEY"] = SECRET_KEY
+#     # app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+#     # # db.init_app(app)
+#     # # migrate.init_app(app, db)
+
+#     app.register_blueprint(med_routes)
+
+#     return app
+
+# if __name__ == "__main__":
+#     my_app = create_app()
+#     my_app.run(debug=True)
