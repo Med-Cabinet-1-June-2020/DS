6d139c37f36e9315bc4d1b7c9db0141e441a01a3
Kristine <ying.kristine.wang@gmail.com>
2020-06-23 18:32:28 -0400

    Loaded table with csv data

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,46 +1,20 @@
 from flask import Flask
 # from web_app.routes.med_routes import med_routes
-import psycopg2
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
 
-DB_NAME=os.getenv("DB_NAME")
-DB_USER=os.getenv("DB_USER")
-DB_PASSWORD=os.getenv("DB_PASSWORD")
-DB_HOST=os.getenv("DB_HOST")
 
-print(DB_NAME,DB_USER, DB_PASSWORD, DB_HOST)
+def create_app():
+    app = Flask(__name__)
 
-connection = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
-print(connection)
+    # app.config["SECRET_KEY"] = SECRET_KEY
+    # app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
+    # # db.init_app(app)
+    # # migrate.init_app(app, db)
 
-cursor = connection.cursor()
-print(cursor)
+    app.register_blueprint(med_routes)
 
-cursor.execute("SELECT * FROM medcabinet1;")
+    return app
 
-result = cursor.fetchall()
-print(result)
-
-# to save transactions
-connection.commit()
-cursor.close()
-connection.close()
-
-# def create_app():
-#     app = Flask(__name__)
-
-#     # app.config["SECRET_KEY"] = SECRET_KEY
-#     # app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
-#     # # db.init_app(app)
-#     # # migrate.init_app(app, db)
-
-#     app.register_blueprint(med_routes)
-
-#     return app
-
-# if __name__ == "__main__":
-#     my_app = create_app()
-#     my_app.run(debug=True)
+if __name__ == "__main__":
+    my_app = create_app()
+    my_app.run(debug=True)
--- web_app/routes/med_routes.py
+++ web_app/routes/med_routes.py
@@ -1,9 +1,45 @@
-from flask import Flask, render_template, jsonify, Blueprint
-from web_app.services.model_service import modelservice
+from flask import Flask, render_template, jsonify, Blueprint, request
+from web_services.model_service import modelservice
+import psycopg2
+import os
+from dotenv import load_dotenv
+import pandas as pd
+from sqlalchemy import create_engine
 
-# from web_app.services import _____
+
+load_dotenv()
+DB_NAME=os.getenv("DB_NAME"),
+DB_USER=os.getenv("DB_USER"),
+DB_PASSWORD=os.getenv("DB_PASSWORD"),
+DB_HOST=os.getenv("DB_HOST")
+DB_URL=os.getenv("DB_URL")
+
+df = pd.read_csv('https://raw.githubusercontent.com/Med-Cabinet-1-June-2020/DS/master/csv/compatible_cannabis.csv')
+
+engine = create_engine(DB_URL)
+con = engine.connect()
+df.to_sql("medcabinet", engine)
+con.close()
+
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST)
 
 med_routes = Blueprint("med_routes", __name__)
+
+@med_routes.route("/recommender", methods=['GET', 'POST'])
+def recommender():
+
+    pg_curs = conn.cursor()
+
+    pg_curs.execute("SELECT * FROM medcabinet;")
+
+    results = modelservice(features=['depression, happiness, insomnia'], pg_curs=pg_curs)
+
+    # to save transactions
+    # connection.commit()
+    # cursor.close()
+    # connection.close()
+
+    return results
 
 @med_routes.route("/dummy_data", methods=['GET'])
 def dummy_data(): 
